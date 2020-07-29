13a88cce12ac5499e99d05d1f0b701d4ae295a1d
antonialucianapires <antonialucianapires@gmail.com>
2020-07-29 00:32:40 -0300

    inserção do endpoint /metrics e da métricar counter

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const prom = require('prom-client');
+const register = prom.register;
 const app = express();
 
-//primeira rota
+//contador
+const client = require('prom-client');
+const { response } = require('express');
+const counter = new client.Counter({
+  name: 'app_request_total',
+  help: 'contador de request',
+  labelNames:['statusCode']
+});
+
+
 app.get('/', (req, res) => {
+
+    //incrementa contador
+    counter.labels('200').inc();
     
     return res.send('Hello, JS!')
 })
 
-app.listen(3001)
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.send(register.metrics());
+})
+
+app.listen(3001);
