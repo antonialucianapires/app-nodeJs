5f696e94ff7af6720aa42df27e5a70c5e2f96e6d
antonialucianapires <antonialucianapires@gmail.com>
2020-07-29 00:49:00 -0300

    inclusão da métrica gauge

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ const register = prom.register;
 const app = express();
 
 //contador
-const client = require('prom-client');
-const { response } = require('express');
-const counter = new client.Counter({
+const counter = new prom.Counter({
   name: 'app_request_total',
   help: 'contador de request',
   labelNames:['statusCode']
 });
 
+//gauge
+const gauge = new prom.Gauge({
+    name: 'app_free_bytes',
+    help: 'contador de request',
+    labelNames:['statusCode']
+})
 
 app.get('/', (req, res) => {
 
-    //incrementa contador
+    //contador
     counter.labels('200').inc();
+
+    //gauge
+    gauge.set(100 * Math.random());
     
     return res.send('Hello, JS!')
 })
